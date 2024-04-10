0f6b85d2bc9df74b68201fa5b7f3838882f9a7fd
Samuel Machado Rodrigues <samuka16mf@gmail.com>
2024-04-10 13:19:34 -0300

    ...

--- index.js
+++ index.js
@@ -1,13 +1,4 @@
 import { iniciarServidor } from "./teste_servidor.js";
 import { iniciarCliente } from "./teste_cliente.js";
 
-<<<<<<< HEAD
 export { WebSocketERServidor, ClienteWebSocketER }
-=======
-async function iniciarTeste() {
-    await iniciarServidor();
-    iniciarCliente();
-}
-
-iniciarTeste();
->>>>>>> eb177c4076e916f9d59f859ed219c721597a4802
